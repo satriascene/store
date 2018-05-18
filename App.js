d3b3f2d6a2f3925730028bc51b580fb0c6a43f97
Deny Bayu Satria <satria.scene@gmail.com>
2018-05-18 09:57:34 +0700

    Tambah Module React native Element

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import {
   View,
   Button
 } from 'react-native';
+import { Avatar } from 'react-native-elements'
 import Analytics from 'appcenter-analytics';
 import Crashes from 'appcenter-crashes';
 import CodePush from 'react-native-code-push';
@@ -25,41 +26,41 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  sendEvent(){
-    Analytics.trackEvent("My Custom Event",{
+  sendEvent() {
+    Analytics.trackEvent("My Custom Event", {
       prop1: new Date().getSeconds()
     })
   }
 
-  nativeCrash(){
+  nativeCrash() {
     Crashes.generateTestCrash();
   }
 
-  jsCrash(){
+  jsCrash() {
     this.func1()
   }
-  func1(){this.func2()}
-  func2(){this.func3()}
-  func3(){this.func4()}
-  func4(){this.func5()}
+  func1() { this.func2() }
+  func2() { this.func3() }
+  func3() { this.func4() }
+  func4() { this.func5() }
 
-  func5(){
-    throw new Error('My uncaught javascript exception');    
+  func5() {
+    throw new Error('My uncaught javascript exception');
   }
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={logs : []}
+    this.state = { logs: [] }
   }
-  codePushSync(){
-    this.setState({logs : ['Started at ' + new Date().getTime()]})
+  codePushSync() {
+    this.setState({ logs: ['Started at ' + new Date().getTime()] })
     CodePush.sync({
-      updateDialog:true,
-      installMode:CodePush.InstallMode.IMMEDIATE
-    }, (status)=>{
-      for(var key in CodePush.SyncStatus){
-        if(status === CodePush.SyncStatus[key]){
-          this.setState(prevState => ({logs:[...prevState.logs,key.replace(/_/g, ' ')]}))
+      updateDialog: true,
+      installMode: CodePush.InstallMode.IMMEDIATE
+    }, (status) => {
+      for (var key in CodePush.SyncStatus) {
+        if (status === CodePush.SyncStatus[key]) {
+          this.setState(prevState => ({ logs: [...prevState.logs, key.replace(/_/g, ' ')] }))
           break
         }
       }
@@ -72,22 +73,48 @@ export default class App extends Component<Props> {
           Welcome to React Native!
         </Text>
         <Text style={styles.codepush}>Added With Code Push</Text>
-        <Button title="Send Event" onPress={()=>this.sendEvent()} />
-        <Button title="Native Crash" onPress={()=>this.nativeCrash()} />
-        <Button title="JS Crash" onPress={()=>this.jsCrash()} />
-        <Button title="Code Push" onPress={()=>this.codePushSync()} />
-        {this.state.logs.map((log,i)=> <Text key={i}>{log}</Text>)}
+        <Button title="Send Event" onPress={() => this.sendEvent()} />
+        <Button title="Native Crash" onPress={() => this.nativeCrash()} />
+        <Button title="JS Crash" onPress={() => this.jsCrash()} />
+        <Button title="Code Push" onPress={() => this.codePushSync()} />
+        {this.state.logs.map((log, i) => <Text key={i}>{log}</Text>)}
         <Text>{JSON.stringify(this.state.logs)}</Text>
+        <Avatar
+          size="small"
+          rounded
+          source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="medium"
+          source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg" }}
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="large"
+          source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg" }}
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="xlarge"
+          rounded
+          source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" }}
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  codepush:{
-    fontSize:25,
-    textAlign:'center',
-    color:'red'
+  codepush: {
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'red'
   },
   container: {
     flex: 1,
